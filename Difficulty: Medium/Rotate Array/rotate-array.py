cca1b21b6ef29bf6f69619d356f698ea0b98ca07
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-05-27 08:38:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        #Your code here
+        n  = len(arr)
+        d = d%n
+        arr[:d] = reversed(arr[:d])
+        arr[d:] = reversed(arr[d:])
+        arr.reverse()
