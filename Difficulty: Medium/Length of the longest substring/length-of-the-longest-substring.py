329c078b5f2e41d9e07b64f8dba678e10c04c4da
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-07-31 21:19:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Length of the longest substring/length-of-the-longest-substring.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+
+class Solution:
+    def longestUniqueSubstring(self, s):
+        # code here
+        hash = [-1]*256
+        n = len(s)
+        l = 0
+        r = 0
+        max_len = 0
+        while r < n:
+            if hash[ord(s[r])] != -1:
+                if hash[ord(s[r])] >= l:
+                    l = hash[ord(s[r])] + 1
+            curr_len = r - l + 1
+            max_len = max(max_len, curr_len)
+            hash[ord(s[r])] = r
+            r += 1
+        return max_len
+                        
