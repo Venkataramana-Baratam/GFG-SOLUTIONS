1db19b71bc79a961c56664bd7ac2ea3d9706fc62
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-08-05 18:18:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def characterReplacement(self, s, k):
+        # Code here
+        l =0
+        r =0 
+        n = len(s)
+        hash =[0]*26
+        maxi_f = 0
+        max_len =0 
+        while r<n:
+            hash[ord(s[r])-ord('A')]+=1
+            maxi_f = max(maxi_f,hash[ord(s[r])-ord('A')])
+            if (r-l+1)-maxi_f>k:
+                hash[ord(s[l])-ord('A')]-=1
+                l+=1
+                maxi_f = 0
+            if (r-l+1)-maxi_f<=k:
+                max_len = max(max_len,r-l+1)
+            r+=1
+        return max_len
+                
