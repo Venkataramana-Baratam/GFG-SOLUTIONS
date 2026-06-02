8e1aadb7a4ebe5528be5c223db0110e4c519f3c9
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2026-06-02 21:46:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Product Subset/max-product-subset.py
+++ Difficulty: Medium/Max Product Subset/max-product-subset.py
@@ -1,32 +1,42 @@
 class Solution:
     def findMaxProduct(self, arr):
+        # code here
+        
+        count_zeros = 0
+        
         count_negative = 0
-        count_zero = 0
-        min_negative = float('inf')
-
+        
         product_without_zero = 1
-
+        
+        min_negative = float('inf')
+        
+        MOD = 10 ** 9 + 7
+        
         if len(arr) == 1:
             return arr[0]
-
         for num in arr:
+            
             if num == 0:
-                count_zero += 1
+                
+                count_zeros += 1
                 continue
-
+            
             if num < 0:
+                
                 count_negative += 1
-                min_negative = min(min_negative, abs(num))
-
+                
+                min_negative = min(min_negative , abs(num))
+                
             product_without_zero *= num
-
-        if count_zero == len(arr):
+            
+        if count_zeros == len(arr):
             return 0
-
+            
         if product_without_zero > 0:
-            return product_without_zero % (10 ** 9 + 7)
-
-        if count_negative == 1 and count_zero + count_negative == len(arr):
+            return product_without_zero % MOD
+            
+        if count_negative == 1 and count_negative + count_zeros == len(arr):
             return 0
-
-        return (product_without_zero // (-min_negative)) % (10 ** 9 + 7)
+            
+        return (product_without_zero // (-min_negative)) % MOD
+                
